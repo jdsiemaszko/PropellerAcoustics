4111c4ac95c674de9a14ba8892ea1cf2a7924a15
jakub <j.d.siemaszko@student.tudelft.nl>
2026-03-17 17:36:09 +0100

    progress

--- scattering_vs_PIN.py
+++ scattering_vs_PIN.py
@@ -225,10 +225,10 @@ origin_prop = np.array([0.0, 0.0, 0.0]) # ... at z=0
 
 
 BLH = blade_l.getBladeLoadingHarmonics()
-# BLH_US = np.zeros_like(BLH)
-# BLH_US[:, 1:, :] = BLH[:, 1:, :]
-# BLH_S = np.zeros_like(BLH)
-# BLH_S[:, 0, :] = BLH[:, 0, :]
+BLH_US = np.zeros_like(BLH)
+BLH_US[:, 1:, :] = BLH[:, 1:, :]
+BLH_S = np.zeros_like(BLH)
+BLH_S[:, 0, :] = BLH[:, 0, :]
 sourceArray = SourceModeArray(BLH=BLH, # loading per unit span
                         B = B,
                         Omega=Omega, gamma =twist,
@@ -253,104 +253,125 @@ c_mps= c0, # speed of sound [m/s]
 nb=NBEAMS # number of beams (irrelevant)
 )
 
-# _____________ PLOTTING & RESULTS _______________
+if __name__ == "__main__":
+    # _____________ PLOTTING & RESULTS _______________
 
-# 1) plot geometry
+    # 1) plot geometry
 
-fig = plt.figure(figsize=(7, 7))
-ax = fig.add_subplot(111, projection="3d")
-sourceArray.plotSelf(fig, ax)
-# ax.set_box_aspect([1, 1, 1])
-ax.set_aspect('equal')
-ax.set_axis_off()
-plt.show()
-plt.close()
+    fig = plt.figure(figsize=(7, 7))
+    ax = fig.add_subplot(111, projection="3d")
+    sourceArray.plotSelf(fig, ax)
+    # ax.set_box_aspect([1, 1, 1])
+    ax.set_aspect('equal')
+    ax.set_axis_off()
+    plt.show()
+    plt.close()
 
-# angular coordinates
-theta = np.linspace(0.0, np.pi, Ntheta, endpoint=True)
-phi   = np.linspace(0.0, 2.0 * np.pi, Nphi, endpoint=True)
+    # angular coordinates
+    theta = np.linspace(0.0, np.pi, Ntheta, endpoint=True)
+    phi   = np.linspace(0.0, 2.0 * np.pi, Nphi, endpoint=True)
 
-# 2D mesh
-theta_m, phi_m = np.meshgrid(theta, phi, indexing='ij')
-# shapes: (Ntheta, Nphi)
+    # 2D mesh
+    theta_m, phi_m = np.meshgrid(theta, phi, indexing='ij')
+    # shapes: (Ntheta, Nphi)
 
-# flatten
-R_arr     = np.full(theta_m.size, R)
-theta_arr = theta_m.ravel()
-phi_arr   = phi_m.ravel()
+    # flatten
+    R_arr     = np.full(theta_m.size, R)
+    theta_arr = theta_m.ravel()
+    phi_arr   = phi_m.ravel()
 
-X = R_arr * np.sin(theta_arr) * np.cos(phi_arr)
-Y = R_arr * np.sin(theta_arr) * np.sin(phi_arr)
-Z = R_arr * np.cos(theta_arr)
+    X = R_arr * np.sin(theta_arr) * np.cos(phi_arr)
+    Y = R_arr * np.sin(theta_arr) * np.sin(phi_arr)
+    Z = R_arr * np.cos(theta_arr)
 
-x_cart = np.array([X, Y, Z])
+    x_cart = np.array([X, Y, Z])
 
-m = np.array([5]) # pick a harmonic with significant beam noise 
+    m = np.array([5]) # pick a harmonic with significant beam noise 
 
-############## save results to a file as the computation takes some time
-p_scattered = sourceArray.getScatteredPressure(x_cart, m)
-np.save('./Data/current/NACA0012_rotor/p_scattered.npy', p_scattered)
-p_direct_blade = sourceArray.getDirectPressure(x_cart, m)
-np.save('./Data/current/NACA0012_rotor/p_direct.npy', p_direct_blade)
+    ############## save results to a file as the computation takes some time
+    # p_scattered = sourceArray.getScatteredPressure(x_cart, m)
+    # np.save('./Data/current/NACA0012_rotor/p_scattered.npy', p_scattered)
+    # p_direct_blade = sourceArray.getDirectPressure(x_cart, m)
+    # np.save('./Data/current/NACA0012_rotor/p_direct.npy', p_direct_blade)
 
-p_scattered = np.load('./Data/current/NACA0012_rotor/p_scattered.npy')
-p_direct_blade = np.load('./Data/current/NACA0012_rotor/p_direct.npy')
+    p_scattered = np.load('./Data/current/NACA0012_rotor/p_scattered.npy')
+    p_direct_blade = np.load('./Data/current/NACA0012_rotor/p_direct.npy')
 
 
-beam_loading = beam_l.getBeamLoadingHarmonics() 
-p_direct_beam, _ = han.getPressureStator(x_cart, m*B, beam_loading) # mind the indexing change for m
-p_direct_blade_hanson, _ = han.getPressureRotor(x_cart, m, BLH) 
+    beam_loading = beam_l.getBeamLoadingHarmonics() 
+    p_direct_beam, _ = han.getPressureStator(x_cart, m*B, beam_loading) # mind the indexing change for m
+    p_direct_blade_hanson, _ = han.getPressureRotor(x_cart, m, BLH) 
 
-# Plot maps:
-# 1) 2D contours
-fig, ((ax1, ax2), (ax3, ax4)) =  plt.subplots(2, 2, figsize=(10, 5), sharex=True, sharey=True)
+    # Plot maps:
+    # 1) 2D contours
+    fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) =  plt.subplots(3, 2, figsize=(10, 5), sharex=True, sharey=True)
 
-VMIN, VMAX = 10, 70
-levels = np.linspace(VMIN, VMAX, 20, endpoint=False)
-fig, ax1 = plot_directivity_contour(
-    np.rad2deg(theta_m), np.rad2deg(phi_m), p_direct_blade, title='blade direct noise', fig=fig, ax=ax1, cmap='jet', levels=levels, xlabel=None,
-)
-fig, ax2 = plot_directivity_contour(
-    np.rad2deg(theta_m), np.rad2deg(phi_m), p_scattered, title='blade scattered noise', fig=fig, ax=ax2, cmap='jet', ylabel=None, levels=levels, xlabel=None,
-)
-fig, ax3 = plot_directivity_contour(
-    np.rad2deg(theta_m), np.rad2deg(phi_m), p_direct_beam, title='beam loading noise', fig=fig, ax=ax3, cmap='jet',  levels=levels,
-)
-# fig, ax4 = plot_directivity_contour(
-#     np.rad2deg(theta_m), np.rad2deg(phi_m), p_direct_beam + p_direct_blade + p_scattered,
-#       title='total loading noise', fig=fig, ax=ax4, cmap='jet',  levels=levels, ylabel=None,
-# )
+    VMIN, VMAX = 40, 65
+    levels = np.linspace(VMIN, VMAX, 20, endpoint=False)
+    fig, ax1 = plot_directivity_contour(
+        np.rad2deg(theta_m), np.rad2deg(phi_m), p_direct_blade, title='blade direct noise (source-mode)', fig=fig, ax=ax1, cmap='jet', levels=levels, xlabel=None,
+    )
+    fig, ax2 = plot_directivity_contour(
+        np.rad2deg(theta_m), np.rad2deg(phi_m), p_scattered, title='blade scattered noise', fig=fig, ax=ax2, cmap='jet', ylabel=None, levels=levels, xlabel=None,
+    )
+    fig, ax3 = plot_directivity_contour(
+        np.rad2deg(theta_m), np.rad2deg(phi_m), p_direct_beam, title='beam loading noise', fig=fig, ax=ax3, cmap='jet',  levels=levels,xlabel=None, 
+    )
 
-fig, ax4 = plot_directivity_contour(
-    np.rad2deg(theta_m), np.rad2deg(phi_m), p_direct_blade_hanson,
-      title='blade direct noise (Hanson)', fig=fig, ax=ax4, cmap='jet',  levels=levels, ylabel=None,
-)
+    fig, ax4 = plot_directivity_contour(
+        np.rad2deg(theta_m), np.rad2deg(phi_m), p_direct_blade_hanson,
+        title='blade direct noise (Hanson)', fig=fig, ax=ax4, cmap='jet',  levels=levels, ylabel=None,  xlabel=None, 
+    )
 
-plt.show()
-plt.close()
+    fig, ax5 = plot_directivity_contour(
+        np.rad2deg(theta_m), np.rad2deg(phi_m), p_direct_blade + p_scattered,
+        title='direct blade + scattered blade', fig=fig, ax=ax5, cmap='jet',  levels=levels, 
+    )
+    fig, ax6 = plot_directivity_contour(
+        np.rad2deg(theta_m), np.rad2deg(phi_m), p_direct_blade + p_direct_beam,
+        title='direct blade + direct beam', fig=fig, ax=ax6, cmap='jet',  levels=levels, ylabel=None,
 
-# 1) 3D blobs
-fig = plt.figure(figsize=(7, 7))
-ax1 = fig.add_subplot(221, projection="3d")
-ax2 = fig.add_subplot(222, projection="3d")
-ax3 = fig.add_subplot(223, projection="3d")
-ax4 = fig.add_subplot(224, projection="3d")
+    )
 
-fig, ax1 = plot_3D_directivity(
-    p_direct_blade, theta_m, phi_m, title='blade direct noise', fig=fig, ax=ax1, valmin=VMIN, valmax=VMAX,
-)
-fig, ax2 = plot_3D_directivity(
-    p_scattered, theta_m, phi_m, title='blade scattered noise', fig=fig, ax=ax2, valmin=VMIN, valmax=VMAX,
-)
-fig, ax3 = plot_3D_directivity(
-    p_direct_beam, theta_m, phi_m, title='beam loading noise', fig=fig, ax=ax3, valmin=VMIN, valmax=VMAX,
-)
-# fig, ax4 = plot_3D_directivity(
-#     p_direct_beam + p_direct_blade + p_scattered, theta_m, phi_m,
-#       title='total loading noise', fig=fig, ax=ax4, valmin=VMIN, valmax=VMAX,
-# )
-fig, ax4 = plot_3D_directivity(
-    p_direct_blade_hanson, theta_m, phi_m,
-      title='blade direct noise (Hanson)', fig=fig, ax=ax4, valmin=VMIN, valmax=VMAX,
-)
-plt.show()
+    plt.show()
+    plt.close()
+
+    VMIN = 10
+    # 1) 3D blobs
+    fig = plt.figure(figsize=(7, 7))
+    ax1 = fig.add_subplot(321, projection="3d")
+    ax2 = fig.add_subplot(322, projection="3d")
+    ax3 = fig.add_subplot(323, projection="3d")
+    ax4 = fig.add_subplot(324, projection="3d")
+    ax5 = fig.add_subplot(325, projection="3d")
+    ax6 = fig.add_subplot(326, projection="3d")
+
+
+    fig, ax1 = plot_3D_directivity(
+        p_direct_blade, theta_m, phi_m, title='blade direct noise', fig=fig, ax=ax1, valmin=VMIN, valmax=VMAX,
+    )
+    fig, ax2 = plot_3D_directivity(
+        p_scattered, theta_m, phi_m, title='blade scattered noise', fig=fig, ax=ax2, valmin=VMIN, valmax=VMAX,
+    )
+    fig, ax3 = plot_3D_directivity(
+        p_direct_beam, theta_m, phi_m, title='beam loading noise', fig=fig, ax=ax3, valmin=VMIN, valmax=VMAX,
+    )
+    # fig, ax4 = plot_3D_directivity(
+    #     p_direct_beam + p_direct_blade + p_scattered, theta_m, phi_m,
+    #       title='total loading noise', fig=fig, ax=ax4, valmin=VMIN, valmax=VMAX,
+    # )
+    fig, ax4 = plot_3D_directivity(
+        p_direct_blade_hanson, theta_m, phi_m,
+        title='blade direct noise (Hanson)', fig=fig, ax=ax4, valmin=VMIN, valmax=VMAX,
+    )
+
+    fig, ax5 = plot_3D_directivity(
+        p_direct_blade + p_scattered, theta_m, phi_m,
+        title='direct blade + scattered blade', fig=fig, ax=ax5, valmin=VMIN, valmax=VMAX,
+    )
+
+    fig, ax5 = plot_3D_directivity(
+        p_direct_blade + p_direct_beam, theta_m, phi_m,
+        title='direct blade + direct beam', fig=fig, ax=ax6, valmin=VMIN, valmax=VMAX,
+    )
+    plt.show()
---
+++ scattering_vs_PIN_spectra.py
@@ -0,0 +1,110 @@
+from scattering_vs_PIN import *
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+
+
+ind_theta = 7        # -60 to 60 in 10
+ind_phi = 0          # 0 to 350 in 10
+datadir = './Experimental/dataverse_files'
+casefile = f'ISAE_2_D{int(1000*D_bras)}_L{int(1000*g)}'
+
+def load_h5(filename):
+    return h5py.File(filename, "r")
+with load_h5(f"{datadir}/{casefile}_autopower.h5") as f:
+    g = f[casefile]
+    freq = np.array(g["frequency_Hz"])
+    ap = g["Autopower"]
+
+    phi_exp = np.array(g["phi_deg"])[0][ind_phi] # azimuth
+    theta_exp = np.array(g["theta_deg"])[0][ind_theta] # polar
+    radius = g["radius_m"][0][0] # float
+
+    BPF = B * RPM / 60
+    data = ap[f"Autopower_RPM_{RPM}_Pa2"][:, ind_theta, ind_phi] # (freq, polar, azimuth), (aziuth=0 = > beam axis, azimuth=9 => 90 deg)
+theta = 90 - theta_exp
+phi = 180 - phi_exp
+print(f'Theta_exp = {theta_exp} deg, Phi_exp = {phi_exp} deg')
+print(f'Theta = {theta} deg, Phi = {phi} deg')
+x_cart = np.array([
+    radius * np.cos(np.deg2rad(phi)) * np.sin(np.deg2rad(theta)),
+    radius * np.sin(np.deg2rad(phi)) * np.sin(np.deg2rad(theta)),
+    radius * np.cos(np.deg2rad(theta)),
+]).reshape((3, 1))
+
+Nr = len(r0)
+
+
+
+ms = np.arange(1, 13, 1) # harmonics to extract
+pSmB_model_rotor = han.getPressureRotor(x_cart, ms, 
+                                    #    blade_l.getBladeLoadingHarmonics()
+                                    BLH_S
+                                       )[0][0]
+
+pUSmB_model_rotor = han.getPressureRotor(x_cart, ms, 
+                                    #    blade_l.getBladeLoadingHarmonics()
+                                    BLH_US
+                                       )[0][0]
+
+ptmB_model_rotor = han.getThicknessNoiseRotor(x_cart, ms, c * np.ones_like(r0), 0.122 * np.ones_like(r0))[0][0] # NACA0012
+BL  =  beam_l.getBeamLoadingHarmonics()
+pmB_model_beam = han.getPressureStator(x_cart, ms*B, BL)[0][0]
+
+pmB_model_rotor_total = pSmB_model_rotor + pUSmB_model_rotor + ptmB_model_rotor
+# pmB_model_total = pSmB_model_rotor + pUSmB_model_rotor + ptmB_model_rotor + pmB_model_beam # assuming coherent
+pmB_model_total = np.sqrt(np.abs(pmB_model_rotor_total)**2 + np.abs(pmB_model_beam)**2) # assuming incoherent
+
+
+p_scattered = sourceArray.getScatteredPressure(x_cart, ms)[0]
+np.save(f'./Data/current/NACA0012_rotor/p_s_spectrum_{ind_theta}_{ind_phi}.npy', p_scattered)
+
+p_scattered = np.load(f'./Data/current/NACA0012_rotor/p_s_spectrum_{ind_theta}_{ind_phi}.npy')
+
+SPL_rotor_S = p_to_SPL(pSmB_model_rotor)
+SPL_rotor_US = p_to_SPL(pUSmB_model_rotor)
+SPL_rotor_total = p_to_SPL(pmB_model_rotor_total)
+SPL_rotor_thickness = p_to_SPL(ptmB_model_rotor)
+SPL_beam = p_to_SPL(pmB_model_beam)
+
+SPL_scattered = p_to_SPL(p_scattered)
+
+
+
+SPL_total = p_to_SPL(pmB_model_total)
+# SPL_total = p_to_SPL(p_rms_total) # same computation
+
+fig, ax = plt.subplots(figsize=(7, 4))
+# ax.plot(freq[0] / BPF, spl_from_autopower(data), label=f"Experimental", color='k', alpha=0.75)
+# fig, ax = plot_BPF_peaks(fig, ax, freq[0] / BPF, spl_from_autopower(data), N0=1, N1= 25, range=0.01, 
+#                          plot_kwargs={
+#                              'color':'k',
+#                              'linestyle':'dashed',
+#                              'alpha':0.75
+#                          })
+# ax.plot(ms, SPL_rotor_S, label=f"Model (rotor, steady loading)", color='r', marker='^')
+# ax.plot(ms, SPL_rotor_US, label=f"Model (rotor, unsteady loading)", color='g', marker='^')
+# ax.plot(ms, SPL_rotor_thickness, label=f"Model (rotor, thickness)", color='b', marker='+')
+# ax.plot(ms, SPL_rotor_total, label=f"Model (rotor, total)", color='y', marker='*', linestyle='dashed')
+# ax.plot(ms, SPL_beam, label=f"Model (beam, loading)", color='m', marker='o')
+# ax.plot(ms, SPL_total, label=f"Model (total)", color='k', marker='s', linestyle='dashed')
+ax.plot(ms, SPL_beam, label=f"Beam PIN", color='r', marker='o')
+ax.plot(ms, SPL_scattered, label=f"Blade Scattering", color='b', marker='s')
+
+
+
+
+ax.legend()
+ax.set_xlabel("$f^+ = f/B/\Omega$ (Hz)")
+ax.set_ylabel("SPL (dB)")
+ax.set_xscale('log')
+# ax.set_yscale('log')
+
+ax.grid(visible=True, which='major', color='k', linestyle='-')
+ax.grid(visible=True, which='minor', color='k', linestyle='--', alpha=0.5)
+
+
+plt.xlim(0.1, 100)
+plt.ylim(0, 70)
+plt.tight_layout()
+plt.show()
